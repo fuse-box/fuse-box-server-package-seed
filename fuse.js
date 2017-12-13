39d3c6a76fed0f4d9e9c6ccecab228abdca5d81a
Ivan Orlov <nchanged@gmail.com>
2017-12-13 14:33:20 +0100

    new build

--- fuse.js
+++ fuse.js
@@ -1,47 +1,97 @@
-const { FuseBox, Sparky, QuantumPlugin } = require("fuse-box");
+const { src, task, exec,  context, tsc, bumpVersion, npmPublish } = require("fuse-box/sparky");
+const { FuseBox, QuantumPlugin, WebIndexPlugin } = require("fuse-box");
 
-let fuse, app, isProduction;
-Sparky.task("config", () => {
-    fuse = FuseBox.init({
-        homeDir: "src",
-        output : "dist/$name.js",
-        target: "server@es6",
-        globals: { 'default': '*' },
-        plugins : [
-            isProduction &&
-                QuantumPlugin({
-                    target : "server",
-                    containedAPI : true,
-                    bakeApiIntoBundle : "app"
-                })
-        ]
+task("default", async context => {
+    await context.clean();
+    await context.development();
+});
+
+task("tsc", async () => {
+    await tsc("src", {
+        target : "esnext",
+        declaration : true,
+        outDir : "dist/"
     });
-
-});
-// development
-// once bundled it will be executed
-// changes are watched
-Sparky.task("default", ["config"], () => {
-    fuse.bundle("dev")
-        .instructions(">[dev.ts]")
-        .watch()
-        .completed(proc => proc.start())
-    return fuse.run();
 });
 
-Sparky.task("set-prod-env", () => {
-    isProduction = true;
+task("test", async context => {
+    await context.test();
+})
+task("dist", async context => {
+    await context.clean();
+    await context.prepareDistFolder();
+    await context.dist("esnext", "es7");
+    await context.dist("es6", "es6");
+    await exec("tsc")
 });
 
-// dist
-// test before
-Sparky.task("dist", ["test", "set-prod-env", "config"], () => {
-    fuse.bundle("app")
-        .instructions("[index.ts]") // add > if you want to execute bundle's entry point once loaded
-    return fuse.run();
-});
+task("publish", async () => {
+    await exec("dist")
+    await npmPublish({path : "dist"});
+})
 
-// test
-Sparky.task("test", ["config"], () => {
-    return fuse.bundle("app").test();
+context(class {
+    getConfig() {
+        return FuseBox.init({
+            homeDir: "src",
+            globals: { 'default': '*' }, // we need to expore index in our bundles
+            target: this.target,
+            output: "dist/$name.js",
+            cache: !this.isProduction,
+            plugins: [!this.isProduction && WebIndexPlugin(),
+                this.isProduction && QuantumPlugin({
+                    containedAPI: true,
+                    ensureES5: false,
+                    target: this.quantumTarget,
+                    uglify: true,
+                    bakeApiIntoBundle: this.bundleName
+                })
+            ]
+        });
+    }
+    async tsc(){
+        await tsc("src", {
+            target : "esnext"
+        });
+    }
+
+    async test(){
+        this.target = "server@esnext";
+        this.bundleName = "app";
+        const fuse = this.getConfig();
+        await fuse.bundle(this.bundleName).test();
+        await fuse.run();
+    }
+
+    async clean() {
+        await src("./dist").clean("dist/").exec();
+    }
+
+    async prepareDistFolder() {
+        await bumpVersion("package.json", {type : "patch"});
+        await src("./package.json").dest("dist/").exec();
+    }
+
+    development() {
+        this.target = "server@esnext";
+        this.bundleName = "app";
+        this.isProduction = false;
+        this.instructions = ">[dev.ts]";
+        const fuse = this.getConfig();
+        const bundle = fuse.bundle(this.bundleName)
+            .instructions(this.instructions).completed(proc => proc.start()).watch();
+        return fuse.run();
+    }
+
+    dist(target, name) {
+        this.target = `server@${target}`;
+        this.bundleName = name;
+        this.isProduction = true;
+        this.quantumTarget = "npm";
+        this.instructions = ">[index.ts]";
+        const fuse = this.getConfig();
+        const bundle = fuse.bundle(this.bundleName)
+            .instructions(this.instructions);
+        return fuse.run();
+    }
 });
